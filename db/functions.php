86d0066ad48709506969a07fb0b11baa5ed299c5
Soyhub <venzolarte@gmail.com>
2022-10-12 00:57:46 +0800

    added schedtime function
    added timein timeout and studentinfo

--- db/functions.php
+++ db/functions.php
@@ -3,7 +3,7 @@
     include ('connect.php');
     
 
-    function insert($info_sid, $info_fname, $info_lname, $info_mname, $info_course, $info_yr_lvl,){
+    function insertStudentInfo($info_sid, $info_fname, $info_lname, $info_mname, $info_course, $info_yr_lvl,){
         global $conn;
 
         $sql = "INSERT INTO `dtp_info`(`info_sid`, `info_fname`, `info_lname`, `info_mname`, `info_course`, `info_yr_lvl`) VALUES ('$info_sid','$info_fname','$info_fname','$info_fname','$info_fname','$info_fname')";
@@ -16,8 +16,31 @@
 
     }
 
-    echo insert(54217, 'venz fredrick', 'venz fredrick', 'venz fredrick', 'IT', '1st year');
+    function insertScheduleTimeIn($sched_date, $sched_tin, $info_id){
+      global $conn;
 
+      $sql = "INSERT INTO `dtp_info`(`sched_date`, `sched_tin`, `info_id`) VALUES ('$sched_date','$sched_tin','$info_id')";
+      
+      if (mysqli_query($conn, $sql)) {
+          echo "New record created successfully";
+        } else {
+          echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+        }
+
+  }
+
+  function insertScheduleTimeOut($sched_date, $sched_tin, $info_id){
+    global $conn;
+
+    $sql = "INSERT INTO `dtp_info`(`sched_date`, `sched_tin`, `info_id`) VALUES ('$sched_date','$sched_tin','$info_id')";
+    
+    if (mysqli_query($conn, $sql)) {
+        echo "New record created successfully";
+      } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+      }
+
+}
 
 
 ?>
