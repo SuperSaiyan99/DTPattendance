ce73ec85945e891d13415acb5dd0792f40aacccf
Soyhub <venzolarte@gmail.com>
2022-10-11 20:21:17 +0800

    kaon
    pakyo ka

---
+++ afternoon.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>DTP | Attendance</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/x-icon" href="img/favicon.ico">
+    <link href="css/afternoon.css" rel="stylesheet">
+  </head>
+  <body>
+
+    <nav>
+      <div class="container">
+        <h1>DTP Attendance Form | Afternoon</h1>
+      </div>
+    </nav>
+
+    <img src="img/dtp_owl.png">
+    
+    <div class="form">
+      <input type="text" placeholder="School ID" id="schoolID" />
+      <input type="text" placeholder="First Name" id="firstName" />
+      <input type="text" placeholder="Middle Name" id="middleName" />
+      <input type="text" placeholder="Last Name" id="lastName" />
+
+      <input type="radio" id="first-yr" /> First Year <br>
+      <input type="radio" id="second-yr" /> Second Year <br>
+      <input type="radio" id="third-yr" /> Third Year <br>
+      <input type="radio" id="fourth-yr" /> Fourth Year <br>
+    </div>
+
+  </body>
+</html>
