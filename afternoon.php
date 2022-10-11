e449364205fc7c99ee40b1d535db7fbaa43522d2
Soyhub <venzolarte@gmail.com>
2022-10-11 20:41:58 +0800

    kaon
    transfer sa localhost

--- afternoon.php
+++ afternoon.php
@@ -5,29 +5,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" type="image/x-icon" href="img/favicon.ico">
-    <link href="css/afternoon.css" rel="stylesheet">
+    <link href="css/morning.css" rel="stylesheet">
   </head>
   <body>
 
     <nav>
       <div class="container">
-        <h1>DTP Attendance Form | Afternoon</h1>
-      </div>
+        <h1>DTP Attendance Form | Morning</h1>
+      </div>s
     </nav>
 
     <img src="img/dtp_owl.png">
     
     <div class="form">
-      <input type="text" placeholder="School ID" id="schoolID" />
-      <input type="text" placeholder="First Name" id="firstName" />
-      <input type="text" placeholder="Middle Name" id="middleName" />
-      <input type="text" placeholder="Last Name" id="lastName" />
+    <input type="text" placeholder="School ID" id="schoolID" maxlength = "6" pattern="[0-9]"/>
+      <input type="text" placeholder="First Name" id="firstName" maxlength="6" pattern="[0-9]"/>
+      <input type="text" placeholder="Middle Name" id="middleName" maxlength="6" pattern="[0-9]"/>
+      <input type="text" placeholder="Last Name" id="lastName"  maxlength="6" pattern="[0-9]"/>
 
       <input type="radio" id="first-yr" /> First Year <br>
       <input type="radio" id="second-yr" /> Second Year <br>
       <input type="radio" id="third-yr" /> Third Year <br>
       <input type="radio" id="fourth-yr" /> Fourth Year <br>
-    </div>
 
+      <button type="submit">Submit</button>
+    </div>
   </body>
 </html>
